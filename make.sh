9bdb38b9926285f067e54b784c1dc8a30536eead
Ievgenii.Mykhalevskyi <Ievgenii.Mykhalevskyi@externals.adidas.com>
2026-01-20 08:41:49 +0200

    bumped version

--- make.sh
+++ make.sh
@@ -5,6 +5,6 @@ npm install
 npm run compile
 vsce package
 
-code --install-extension vscode-swiftformat-xcode-1.7.7.vsix
+code --install-extension vscode-swiftformat-xcode-1.7.8.vsix
 
 vsce publish
